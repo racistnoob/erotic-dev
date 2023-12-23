ab78a6b5db5cfdf2e58e31615e5228a843cb269a
eroticDEV <154284981+eroticDEV@users.noreply.github.com>
2023-12-23 09:53:53 -0800

    Update thread.lua

--- resources/[Bimbo]/core/client/thread.lua
+++ resources/[Bimbo]/core/client/thread.lua
@@ -22,15 +22,6 @@ Citizen.CreateThread(function()
         end
 
         BlockWeaponWheelThisFrame()
-
-        DisableControlAction(0, 199, true) 
-        DisableControlAction(0, 37, true)
-        DisableControlAction(0, 140, true)
-        DisableControlAction(0, 141, true)
-        DisableControlAction(0, 142, true)
-        DisableControlAction(0, 257, true)
-        DisableControlAction(0, 263, true)
-        DisableControlAction(0, 264, true)
     end
 end)
 
@@ -45,5 +36,19 @@ Citizen.CreateThread(function()
         else
             Citizen.Wait(3000)
         end
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        DisableControlAction(0, 199, true)
+        DisableControlAction(0, 37, true)
+        DisableControlAction(0, 140, true)
+        DisableControlAction(0, 141, true)
+        DisableControlAction(0, 142, true)
+        DisableControlAction(0, 257, true)
+        DisableControlAction(0, 263, true)
+        DisableControlAction(0, 264, true)
     end
 end)
