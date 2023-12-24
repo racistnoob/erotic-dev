1a9bfe8289f2b86ffe12e95e486bcac98f6fdc2e
Bimbo <88099229+BimboXl@users.noreply.github.com>
2023-12-24 00:55:25 -0600

    Hud Changes

---
+++ resources/[Bimbo]/hud/client/cl_main.lua
@@ -0,0 +1,36 @@
+Citizen.CreateThread(function()
+    while true do
+        Wait(100)
+
+        local ped = GetPlayerPed(-1)
+        local health = GetEntityHealth(ped) / 2
+        local armor = GetPedArmour(ped)
+
+        --print("Health: " .. health .. ", Armor: " .. armor)
+
+        if health < 0 then health = 0 end
+
+        SendNUIMessage({
+            type = 'updateHUD',
+            health = health,
+            armor = armor
+        })
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+        DisplayAmmoThisFrame(false) -- Disable ammo display
+        HideHudComponentThisFrame(1) -- Wanted stars
+        HideHudComponentThisFrame(2) -- Weapon icon
+        HideHudComponentThisFrame(3) -- Cash
+        HideHudComponentThisFrame(4) -- MP Cash
+        HideHudComponentThisFrame(6) -- Vehicle name
+        HideHudComponentThisFrame(7)  -- Area name, hides armor
+        HideHudComponentThisFrame(9)  -- Street name, hides health
+        HideHudComponentThisFrame(8) -- Vehicle class
+        HideHudComponentThisFrame(9)  -- Street name, hides health
+        HideHudComponentThisFrame(13) -- Cash change
+    end
+end)
---
+++ resources/[Bimbo]/hud/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'bimbo'
+version '1.0.0'
+
+client_scripts {
+    "client/**",
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
