2064fccf9c01927fffd350212a152c4a0d698406
Bimbo <88099229+BimboXl@users.noreply.github.com>
2023-12-23 18:29:16 -0600

    framework changes

--- resources/[Framework]/drp/server/Old/events.lua
+++
@@ -1,9 +0,0 @@
-RegisterNetEvent('deletevehicle:server')
-AddEventHandler('deletevehicle:server', function(vehicleNet)
-	if vehicleNet then
-		local vehicle = NetworkGetEntityFromNetworkId(vehicleNet)
-		if DoesEntityExist(vehicle) then
-			DeleteEntity(vehicle)
-		end 
-	end
-end)
