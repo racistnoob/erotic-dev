a098de2be31c1a8c48350385602e514343ab8313
Bimbo <88099229+BimboXl@users.noreply.github.com>
2023-12-30 04:06:25 -0600

    new crouch

--- resources/[Bimbo]/core/client/crouch.lua
+++ resources/[Bimbo]/core/client/crouch.lua
@@ -3,40 +3,41 @@ local ped = 0
 local exists = DoesEntityExist(ped)
 local isDead = IsEntityDead(ped)
 
-
-CreateThread( function()
-    Wait(1)
-    
-    DisableControlAction(0, 36, true)
-end)
-
-RegisterCommand('toggleCrouch', function()
-    local ped = PlayerPedId()
-    local isDead = IsEntityDead(ped)
-    if not isDead then 
-        
-        if not IsPauseMenuActive() then 
-            if IsDisabledControlJustPressed(0, 36) then 
-                RequestAnimSet("move_ped_crouched")
-                while not HasAnimSetLoaded("move_ped_crouched") do 
-                    Citizen.Wait(100)
-                end 
-
-                if crouched then 
-                    ResetPedMovementClipset(ped, 0)
-                    crouched = false 
-                else
-                    SetPedMovementClipset(ped, "move_ped_crouched", 0.25)
-                    crouched = true 
-                end 
-            end
-        end
-
-        if IsPlayerFreeAiming(PlayerId()) then
-            ResetPedMovementClipset(ped, 0)
-            crouched = false
-        end
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+        ped = PlayerPedId()
+        exists = DoesEntityExist(ped)
+        isDead = IsEntityDead(ped)
     end
 end)
 
-RegisterKeyMapping('toggleCrouch', 'Toggle Crouch', 'keyboard', 'LCONTROL')   
+Citizen.CreateThread(function()
+    while true do 
+        Citizen.Wait(1)
+        if exists and not isDead then 
+            DisableControlAction(0, 36, true)
+            if not IsPauseMenuActive() then 
+                if IsDisabledControlJustPressed(0, 36) then 
+                    RequestAnimSet("move_ped_crouched")
+                    while not HasAnimSetLoaded("move_ped_crouched") do 
+                        Citizen.Wait(100)
+                    end 
+
+                    if crouched then 
+                        ResetPedMovementClipset(ped, 0)
+                        crouched = false 
+                    else
+                        SetPedMovementClipset(ped, "move_ped_crouched", 0.25)
+                        crouched = true 
+                    end 
+                end
+            end
+
+            if IsPlayerFreeAiming(PlayerId()) then
+                ResetPedMovementClipset(ped, 0)
+                crouched = false
+            end
+        end 
+    end
+end)
