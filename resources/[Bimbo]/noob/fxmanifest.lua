88498c9201af8457ab1f36d8a0df22f01c6393f5
noob <65266187+racistnoob@users.noreply.github.com>
2024-01-19 22:42:42 +0000

    ammo images in inventory

--- resources/[Bimbo]/noob/fxmanifest.lua
+++ resources/[Bimbo]/noob/fxmanifest.lua
@@ -4,6 +4,12 @@ game 'gta5'
 author 'noob'
 version '1.0.0'
 
+ui_page "client/game_view/index.html"
+
+files {
+  "client/game_view/**/*",
+}
+
 client_scripts {
     "@PolyZone/client.lua",
     "@PolyZone/BoxZone.lua",
