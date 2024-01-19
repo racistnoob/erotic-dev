e5d14f221802689e302fc618b08cf9be473f629c
noob <65266187+racistnoob@users.noreply.github.com>
2024-01-19 22:50:36 +0000

    reverting thing pushed on accident

--- resources/[Bimbo]/noob/fxmanifest.lua
+++ resources/[Bimbo]/noob/fxmanifest.lua
@@ -4,12 +4,6 @@ game 'gta5'
 author 'noob'
 version '1.0.0'
 
-ui_page "client/game_view/index.html"
-
-files {
-  "client/game_view/**/*",
-}
-
 client_scripts {
     "@PolyZone/client.lua",
     "@PolyZone/BoxZone.lua",
