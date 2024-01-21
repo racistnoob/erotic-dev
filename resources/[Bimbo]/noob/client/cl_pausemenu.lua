07185f48afa5b0c60b9965819bcf536ffdc978f8
noob <65266187+racistnoob@users.noreply.github.com>
2024-01-21 22:17:59 +0000

    pausemenu without custom stream

--- resources/[Bimbo]/noob/client/cl_pausemenu.lua
+++ resources/[Bimbo]/noob/client/cl_pausemenu.lua
@@ -1,13 +1,8 @@
-Config = Config or {}
+CreateThread(function()
+    AddTextEntry("FE_THDR_GTAO", "Erotic PVP")      
+    ReplaceHudColourWithRgba(116, 144, 138, 255, 255)
+end)
 
-Config.RGBA = {
-    LINE = { -- Line over the Options
-        ["RED"] = 144,
-        ["GREEN"] = 138,
-        ["BLUE"] = 255,
-        ["ALPHA"] = 255,
-    }
-}
 
 local open = false
 RegisterCommand('pausemenu', function()
@@ -16,24 +11,11 @@ RegisterCommand('pausemenu', function()
         repeat
             open = true
             SetScriptGfxDrawBehindPausemenu(true)
-            DrawSprite("ps_pause", "background", 0.5, 0.5, 1.0, 1.0, 0, 255, 230, 255, 100)
+            DrawSprite("", "", 0.5, 0.5, 1.0, 1.0, 0, 144, 138, 255, 100)
             Wait(3)
         until not IsPauseMenuActive()
         open = false
-        SetStreamedTextureDictAsNoLongerNeeded('ps_pause')
     end
 end)
 
 RegisterKeyMapping('pausemenu', 'Pause Menu', 'keyboard', 'ESCAPE')
-
-CreateThread(function()
-    AddTextEntry("FE_THDR_GTAO", "Erotic PVP")
-        
-    RequestStreamedTextureDict('pause', true)
-
-    ReplaceHudColourWithRgba(116, 144, 138, 255, 255)
-
-    while not HasStreamedTextureDictLoaded("pause") do
-        Wait(100)
-    end
-end)
