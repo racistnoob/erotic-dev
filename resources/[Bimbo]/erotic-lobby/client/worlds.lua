e0504bdd739baf1c41c8b454082e9d71b00c5b3c
Bimbo <88099229+BimboXl@users.noreply.github.com>
2024-01-22 19:52:17 -0600

    updated data

--- resources/[Bimbo]/erotic-lobby/client/worlds.lua
+++ resources/[Bimbo]/erotic-lobby/client/worlds.lua
@@ -8,65 +8,33 @@ local defaultSpawn = {
 }
 
 local worlds = {
-    -- { startID = 0, endID = 0, settings = { 
-    --     recoilMode = "roleplay", 
-    --     firstPersonVehicle = true, 
-    --     hsMulti = false,
-    --     spawn = {     
-    --     x = 0,
-    --     y = 0,
-    --     z = 0,
-    --     h = 0
-    -- }
-    -- }},
-    { startID = 1, endID = 1, settings = { 
-        recoilMode = "roleplay", 
+    { ID = 1, settings = {
+        name = 'Southside #1',
+        tags = {'FPS Mode', 'Light Recoil'},
+        recoilMode = "roleplay",
         firstPersonVehicle = true, 
         hsMulti = false,
     }},
-    { startID = 2, endID = 2, settings = { 
-        recoilMode = "roleplay", 
+    { ID = 2, settings = {
+        name = 'FFA Bunker',
+		tags = {'FPS Mode', 'Light Recoil', 'FFA'},
+        recoilMode = "roleplay",
         firstPersonVehicle = true, 
-        hsMulti = true,
+        hsMulti = false,
         spawningcars = false,   
         RandomSpawns = {
             [1] = {x = 895.6183, y = -3245.4922,  z = -98.2509, h = 143.0110},
-            [2] = {x = 843.3173, y = -3235.2666, z = -98.6991, h = 143.0110},
-            [3] ={x = 856.9405, y = -3212.1631, z = -98.4711, h = 143.0110},
-            [4] ={x = 872.1874, y = -3211.3135, z = -97.2462, h = 143.0110},
-            [5] ={x = 867.2796, y = -3186.1096, z = -96.2488, h = 143.0110},
-            [6] ={x = 900.2949, y = -3182.2495, z = -97.0675, h = 143.0110},
-            [7] ={x = 904.9290, y = -3199.6267, z = -97.1880, h = 143.0110},
-            [8] ={x = 909.7304, y = -3236.9119, z = -98.2942, h = 143.0110},
-            [9] ={x = 945.9636, y = -3210.4885, z = -98.2774, h = 143.0110},
-            [10] ={x = 918.3181, y = -3201.2510, z = -98.2621, h = 143.0110},
-            [11] ={x = 906.0173, y = -3230.5940, z = -98.2944, h = 143.0110},
-            [12] = {x = 884.8878, y = -3211.9189, z = -98.1962, h = 143.0110},
+            -- Add other spawn points as needed
         },                                                         
-    },},
-    { startID = 3, endID = 4, settings = { 
-        recoilMode = "roleplay", 
-        firstPersonVehicle = true, 
-        nonstopCombat = false,
-        hsMulti = true 
     }},
-    { startID = 5, endID = 6, settings = { 
-        recoilMode = "envy", 
-        firstPersonVehicle = true, 
-        hsMulti = true 
-    }},
-    { startID = 7, endID = 8, settings = { 
-        recoilMode = "envy", 
-        firstPersonVehicle = true, 
-        hsMulti = true 
-    }},
-    { startID = 9, endID = 10, settings = { 
+    { ID = 6, settings = {
+        name = 'Casino Deluxo',
+		tags = {'FPS Mode', 'Medium Recoil', 'Deluxo', 'Headshots'},
         recoilMode = "roleplay2",
         firstPersonVehicle = true, 
         hsMulti = false,
         spawningcars = false,
         CarRagdoll = true,
-        kit = {'snipers', 'trick2'},
         spawn = {
             x = 770.5748, 
             y = -233.9927, 
@@ -74,29 +42,51 @@ local worlds = {
             h = 354.6606
         }
     }},
-    { startID = 11, endID = 12, settings = { 
-        recoilMode = "qb", 
+    { ID = 3, settings = {
+        name = 'Southside #2',
+        tags = {'FPS Mode', 'Light Recoil', 'Headshots'},
+        recoilMode = "roleplay",
+        firstPersonVehicle = true, 
+        nonstopCombat = false,
+        hsMulti = true 
+    }},
+    { ID = 4, settings = {
+        name = 'Southside #3',
+		tags = {'FPS Mode', 'Envy Recoil'},
+        recoilMode = "envy", 
+        firstPersonVehicle = true, 
+        hsMulti = false 
+    }},
+    { ID = 5, settings = {
+        name = 'Southside #4',
+		tags = {'FPS Mode', 'Envy Recoil', 'Headshots'},
+        recoilMode = "envy", 
         firstPersonVehicle = true, 
         hsMulti = true 
     }},
-    { startID = 13, endID = 16, settings = { 
-        recoilMode = "roleplay2", 
-        firstPersonVehicle = false, 
-        hsMulti = true
-    }},
-    { startID = 17, endID = 20, settings = { 
+    { ID = 7, settings = {
+        name = 'Southside #5',
+		tags = {'Third Person', 'Light Recoil'},
         recoilMode = "roleplay3", 
         firstPersonVehicle = false, 
         hsMulti = false
     }},
 }
 
+RegisterNetEvent('erotic-lobby:updateLobbies')
+AddEventHandler('erotic-lobby:updateLobbies', function()
+    SendNUIMessage({
+        type = "updateLobbies",
+        lobbies = worlds,
+    })
+end)
+
 function switchWorld(worldID)
     worldID = tonumber(worldID)
     if worldID then
         local worldSettings, worldSpawn = nil, nil
         for _, world in pairs(worlds) do
-            if worldID >= world.startID and worldID <= world.endID then
+            if world.ID == worldID then
                 worldSettings = world.settings
                 worldSpawn = worldSettings.spawn or defaultSpawn
 
@@ -136,7 +126,7 @@ end
 function getCurrentWorldDeathSpot()
     if currentWorldID then
         for _, world in pairs(worlds) do
-            if currentWorldID >= world.startID and currentWorldID <= world.endID then
+            if world.ID == currentWorldID then
                 if world.settings.RandomSpawns then
                     return world.settings.RandomSpawns[math.random(1, #world.settings.RandomSpawns)]
                 end
@@ -154,8 +144,3 @@ exports("switchWorld", switchWorld)
 exports("getCurrentWorld", function()
     return currentWorldID
 end)
-
--- RegisterNUICallback("getWorldsData", function(data, callback)
---     local worldsData = getWorldsData() -- Assuming you have the getWorldsData function defined
---     callback(worldsData)
--- end)
--- resources/[Bimbo]/erotic-lobby/web/src/components/App.tsx
+++ resources/[Bimbo]/erotic-lobby/web/src/components/App.tsx
@@ -3,40 +3,45 @@ import './App.css';
 import { fetchNui } from '../utils/fetchNui';
 
 interface Lobby {
-  id: number;
-  name: string;
-  settings: string[];
+  ID: number;
+  settings: {
+    name: string;
+    recoilMode: string;
+    tags: string[];
+    firstPersonVehicle: boolean;
+    hsMulti: boolean;
+    spawningcars?: boolean;
+    RandomSpawns?: { x: number; y: number; z: number; h: number }[];
+  };
   playerCount: number;
 }
 
 const App: React.FC = () => {
-  const lobbiesRef = useRef<Lobby[]>([
-
-  ]);
-
-  const handleUpdateLobbies = (event: MessageEvent) => {
-    if (event.data.type === 'updateLobbies') {
+    const lobbiesRef = useRef<Lobby[]>([]);
+  
+    const handleUpdateLobbies = (event: MessageEvent) => {
+      if (event.data.type === 'updateLobbies') {
         const receivedLobbies: Lobby[] = event.data.lobbies;
-
-        // Preserve existing player counts
+  
         const updatedLobbies = receivedLobbies.map((receivedLobby: Lobby) => {
-            const existingLobby = lobbiesRef.current.find(lobby => lobby.id === receivedLobby.id);
-
-            if (existingLobby) {
-                // Lobby already exists, update only properties that need to be changed
-                return { ...existingLobby, name: receivedLobby.name, settings: receivedLobby.settings };
-            } else {
-                // Lobby doesn't exist, add it to the array
-                return receivedLobby;
-            }
+          const existingLobby = lobbiesRef.current.find(lobby => lobby.ID === receivedLobby.ID);
+  
+          if (existingLobby) {
+            return {
+              ...existingLobby,
+              name: receivedLobby.settings.name,
+              settings: receivedLobby.settings,
+            };
+          } else {
+            return receivedLobby;
+          }
         });
-
+  
         lobbiesRef.current = updatedLobbies;
-
-        // Update filteredLobbies to display all lobbies
+  
         setFilteredLobbies(updatedLobbies);
-    }
-};
+      }
+    };
   
   const handlePlayerCountUpdate = (event: MessageEvent) => {
     console.log('Received Player Count Update:', event.data);
@@ -46,7 +51,7 @@ const App: React.FC = () => {
         const worldID = event.data.worldId;
 
         lobbiesRef.current = lobbiesRef.current.map((lobby) => {
-            if (lobby.id === worldID) {
+            if (lobby.ID === worldID) {
                 return { ...lobby, playerCount: newPlayerCount };
             }
             return lobby;
@@ -54,14 +59,13 @@ const App: React.FC = () => {
 
         setFilteredLobbies((prevFilteredLobbies) =>
             prevFilteredLobbies.map((lobby) => {
-                if (lobby.id === worldID) {
+                if (lobby.ID === worldID) {
                     return { ...lobby, playerCount: newPlayerCount };
                 }
                 return lobby;
             })
         );
 
-        // Log lobby data before sending to NUI
         console.log('Updated Lobby Data:', lobbiesRef.current);
     }
   };
@@ -80,73 +84,35 @@ const App: React.FC = () => {
       });
   };
 
-  type FilterSettings = { [key: string]: boolean };
-  const [filterSettings, setFilterSettings] = useState<FilterSettings>({
-    'FPS Mode': false,
-    'Deluxo': false,
-    'FFA': false,
-    'Third Person': false,
-    'Headshots': false,
-  });
-  const [selectedRecoil, setSelectedRecoil] = useState('');
-
-  const handleFilterChange = (setting: string) => {
-    setFilterSettings({ ...filterSettings, [setting]: !filterSettings[setting] });
-  };
-
-  const handleRecoilChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedRecoil(e.target.value);
-  };
-
   const [filteredLobbies, setFilteredLobbies] = useState<Lobby[]>([]);
 
   useEffect(() => {
     window.addEventListener('message', handleUpdateLobbies);
 
-    // Cleanup function
     return () => {
       window.removeEventListener('message', handleUpdateLobbies);
     };
-  }, []); // Make sure to have an empty dependency array to run the effect only once on mount
+  }, []);
 
   useEffect(() => {
     window.addEventListener('message', handlePlayerCountUpdate);
   
-    // Cleanup function
     return () => {
       window.removeEventListener('message', handlePlayerCountUpdate);
     };
-  }, []); // Make sure to have an empty dependency array to run the effect only once on mount  
+  }, []);
 
   return (
     <div className='overlay'>
       <div className="lobby-container">
-        <div className="options">
-        {Object.keys(filterSettings).map(setting => (
-          <label key={setting}>
-            <input className='lobby-checkbox'
-              type="checkbox"
-              checked={filterSettings[setting]}
-              onChange={() => handleFilterChange(setting)}
-            />
-            {setting}
-          </label>
-        ))}
-        <select value={selectedRecoil} onChange={handleRecoilChange} className='lobby-select'> 
-          <option value="">All Recoils</option>
-          <option value="Envy Recoil">Envy Recoil</option>
-          <option value="Light Recoil">Light Recoil</option>
-          <option value="Medium Recoil">Medium Recoil</option>
-          <option value="High Recoil">High Recoil</option>
-        </select>
-        </div>
+        {/* ... (other code) */}
         <div className="lobby-list">
           {filteredLobbies.map((lobby) => (
-            <div key={lobby.id} className="lobby-item" onClick={() => handleJoinLobby(lobby.id)}>
-              <h3 className="lobby-title">{lobby.name}</h3>
+            <div key={lobby.ID} className="lobby-item" onClick={() => handleJoinLobby(lobby.ID)}>
+              <h3 className="lobby-title">{lobby.settings.name}</h3>
               <div className="lobby-settings">
-                {lobby.settings.map((setting, index) => (
-                  <p key={index} className="lobby-setting">{setting}</p>
+                {lobby.settings.tags.map((tag, index) => (
+                  <p key={index} className="lobby-setting">{tag}</p>
                 ))}
               </div>
               <p className="lobby-player-count">🧑‍🤝‍🧑/ {lobby.playerCount || 0}</p>
